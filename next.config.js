1d9822f8598065b0fa7ccca108277d72e40f1134
bigfang <bitair@gmail.com>
2022-06-16 10:32:13 +0800

    增加 sass 依赖

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const path = require('path')
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin())
     return config
