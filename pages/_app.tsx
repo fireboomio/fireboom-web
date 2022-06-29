47b09011de7a98a478f14da7ebbdccd1f7535b36
bigfang <bitair@gmail.com>
2022-06-29 14:06:41 +0800

    修改 windi 导入位置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import '@/styles/globals.css'
-// eslint-disable-next-line import/no-unresolved
-import 'windi.css'
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/layout'
