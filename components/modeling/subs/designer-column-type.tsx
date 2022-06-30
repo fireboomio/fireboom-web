955c5c7996229cac14a1d738923233fd4659ee60
bigfang <bitair@gmail.com>
2022-06-30 19:02:42 +0800

    搜索，更新类型

--- components/modeling/subs/designer-column-type.tsx
+++ components/modeling/subs/designer-column-type.tsx
@@ -1,11 +1,12 @@
 import { Input, Popover } from 'antd'
+import { useCallback } from 'react'
 import { useImmer } from 'use-immer'
 
 interface Props {
   data: string
 }
 
-const TYPES = [
+const BASE_TYPES = [
   'String',
   'Boolean',
   'Int',
@@ -19,27 +20,34 @@ const TYPES = [
 
 const { Search } = Input
 
-function PopoverContent() {
-  const [types, setTypes] = useImmer(TYPES)
+interface PropoverProps {
+  onClick: (value: string) => void
+}
 
-  function onSearch(value: string) {
-    setTypes(types.filter((type) => type.toLowerCase().includes(value)))
-  }
+function PopoverContent({ onClick }: PropoverProps) {
+  const [types, setTypes] = useImmer(BASE_TYPES)
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setTypes(types.filter((type) => type.toLowerCase().includes(e.target.value)))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
       <Search
+        autoFocus
         className="h-4"
-        enterButton={false}
         size="small"
         placeholder="Basic usage"
-        onSearch={onSearch}
+        onChange={onChange}
       />
       <div className="h-1px w-full border border-solid border-[#5F6269] border-opacity-10 my-2 mt-3" />
       <ul>
         {types.map((type, idx) => (
           <li key={idx}>
-            <div className="my-2 cursor-pointer">{type}</div>
+            <div className="my-2 cursor-pointer hover:bg-[#F8F8F9]" onClick={() => onClick(type)}>
+              {type}
+            </div>
           </li>
         ))}
       </ul>
@@ -48,10 +56,21 @@ function PopoverContent() {
 }
 
 export default function ModelDesignerColumnType({ data }: Props) {
+  const [value, setValue] = useImmer(data)
+
+  function handleSelect(value: string) {
+    setValue(value)
+  }
+
   return (
     <div className="h-6 w-full max-w-150px hover:bg-[#F8F8F9] cursor-pointer">
-      <Popover content={PopoverContent} title="字段类型" trigger="click">
-        {data}
+      <Popover
+        placement="right"
+        content={PopoverContent({ onClick: handleSelect })}
+        title="字段类型"
+        trigger="click"
+      >
+        {value}
       </Popover>
     </div>
   )
