c53a93eef178439cd4d51cad2cebd71c20723d51
bigfang <bitair@gmail.com>
2022-07-21 09:34:06 +0800

    remove useless code

--- components/iconfont/index.tsx
+++ components/iconfont/index.tsx
@@ -1,11 +1,4 @@
 import { createFromIconfontCN } from '@ant-design/icons/lib'
-// import { IconBaseProps } from '@ant-design/icons/lib/components/Icon'
-
-export type IconFontType = string
-
-// interface IconFontProps extends Omit<IconBaseProps, 'type'> {
-//   type: IconFontType
-// }
 
 const IconFont = createFromIconfontCN({
   scriptUrl: '/iconfont/iconfont.js',
