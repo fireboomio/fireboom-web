92502d4ba04dc5f8398d305042d46295ab095575
bigfang <bitair@gmail.com>
2022-08-11 15:01:20 +0800

    修复类型错误

--- pages/apimanage/index.tsx
+++ pages/apimanage/index.tsx
@@ -1,13 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import {
   CaretRightOutlined,
   DownOutlined,
@@ -30,7 +20,6 @@ import {
   Button,
 } from 'antd'
 import { Key } from 'antd/lib/table/interface'
-import type { DataNode } from 'antd/lib/tree'
 import Head from 'next/head'
 import { FC, useCallback, useEffect, useState, useReducer } from 'react'
 import { useImmer } from 'use-immer'
@@ -42,7 +31,7 @@ import Setting from '@/components/apimanage/Setting'
 import { DatasourcePannel, DatasourceContainer } from '@/components/datasource'
 import IconFont from '@/components/iconfont'
 import RcTab from '@/components/rc-tab'
-import type { DirTree, operationResp } from '@/interfaces/apimanage'
+import type { DirTreeNode, OperationResp } from '@/interfaces/apimanage'
 import type { DatasourceResp } from '@/interfaces/datasource'
 import {
   DatasourceContext,
@@ -68,29 +57,32 @@ const tabs = [
   { title: '调用', key: '4' },
 ]
 
-function convertToTree(data: operationResp[] | null, lv = '0'): DirTree[] | null {
-  if (!data) return null
+function convertToTree(data: OperationResp[] | null, lv = '0'): DirTreeNode[] {
+  if (!data) return []
   return data.map((x, idx) => ({
+    ...x,
     key: `${lv}-${idx}`,
+    id: x.id,
     title: x.title.split('/')[x.title.split('/').length - 1].replace(/\.graphql(\.off)?$/, ''),
     path: x.title.split('/').slice(0, x.title.split('/').length).join('/'),
+    // .replace(/\.graphql(\.off)?$/, ''),
     children: convertToTree(x.children, `${lv}-${idx}`),
     originTitle: x.title,
     enable: x.enable,
   }))
 }
 
-function findNode(key: string, data: DataNode[] | undefined): DataNode | undefined {
+function findNode(key: string, data: DirTreeNode[] | undefined): DirTreeNode | undefined {
   let rv
 
-  const inner = (key: string, nodes: DataNode[] | undefined) => {
+  const inner = (key: string, nodes: DirTreeNode[] | undefined) => {
     if (!nodes) return undefined
     nodes.find((x) => {
       if (x.key === key) {
         rv = x
         return x
       } else {
-        return inner(key, x.children)
+        return inner(key, x.children ?? undefined)
       }
     })
   }
@@ -99,11 +91,11 @@ function findNode(key: string, data: DataNode[] | undefined): DataNode | undefin
   return rv
 }
 
-function getNodeFamily(key: string, data: DataNode[] | undefined) {
-  let parent: DataNode | undefined
-  let curr: DataNode | undefined
+function getNodeFamily(key: string, data: DirTreeNode[] | undefined) {
+  let parent: DirTreeNode | undefined
+  let curr: DirTreeNode | undefined
 
-  const inner = (key: string, nodes: DataNode[] | undefined) => {
+  const inner = (key: string, nodes: DirTreeNode[] | undefined) => {
     if (!nodes) return []
     nodes.find((x) => {
       if (x.key === key) {
@@ -111,7 +103,7 @@ function getNodeFamily(key: string, data: DataNode[] | undefined) {
         return [parent, curr]
       } else {
         if (x.children) parent = x
-        return inner(key, x.children)
+        return inner(key, x.children ?? undefined)
       }
     })
   }
@@ -121,21 +113,22 @@ function getNodeFamily(key: string, data: DataNode[] | undefined) {
 }
 
 const ApiManage: FC<ApiManageProps> = () => {
-  const [addType, setAddType] = useState<'文件' | '目录' | '编辑'>(null)
-  const [treeData, setTreeData] = useState<DataNode[]>(null!)
+  const [addType, setAddType] = useState<'文件' | '目录' | '编辑' | null>(null)
+  const [treeData, setTreeData] = useState<DirTreeNode[]>(null!)
   const [selectedKey, setSelectedKey] = useState<string>('')
-  const [curEditingNode, setCurEditingNode] = useState<DataNode | null>(null)
+  const [curEditingNode, setCurEditingNode] = useState<DirTreeNode | null>(null)
   const [inputValue, setInputValue] = useState('')
   const [activeKey, setActiveKey] = useState<string>('0')
   const [refreshFlag, setRefreshFlag] = useState<boolean>()
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [query, setQuery] = useState<string>()
+  const [path, setPath] = useState<string>()
 
   //datasource逻辑-----
   const [showDatasource, setShowDatasource] = useImmer(false)
   const [datasource, dispatch] = useReducer(datasourceReducer, [])
   const [showType, setShowType] = useImmer('data')
-  const [currDBId, setCurrDBId] = useImmer(null as number | null | undefined)
+  const [currDBId, setCurrDBId] = useImmer<number | null | undefined>(null)
 
   useEffect(() => {
     requests
@@ -176,12 +169,12 @@ const ApiManage: FC<ApiManageProps> = () => {
   //-----datasource逻辑
 
   useEffect(() => {
-    getFetcher<operationResp[]>('/operateApi')
+    getFetcher<OperationResp[]>('/operateApi')
       // .then((x) => {
       //   console.log(convertToTree(x))
       //   return x
       // })
-      .then((res) => setTreeData(convertToTree(res) as DataNode[]))
+      .then((res) => setTreeData(convertToTree(res)))
       .catch((err: Error) => {
         throw err
       })
@@ -197,14 +190,14 @@ const ApiManage: FC<ApiManageProps> = () => {
       const basePath = curEditingNode.path
         .split('/')
         .slice(0, curEditingNode.path.split('/').length - 1)
-        .join('/') as string
+        .join('/')
       const newPath = `${basePath}/${inputValue}`
 
       void requests
-        .put('/operateApi/rename', {
+        .put(`/operateApi/rename/${curEditingNode.id}`, {
           oldPath: curEditingNode.path,
           newPath: newPath,
-          enable: !curEditingNode.disable,
+          enable: curEditingNode.enable,
         })
         .then((res) => {
           if (res) {
@@ -220,7 +213,7 @@ const ApiManage: FC<ApiManageProps> = () => {
         return
       }
       curEditingNode.title = inputValue
-      const curPath = `${(curEditingNode.path as string) || '/'}${curEditingNode.title as string}`
+      const curPath = `${curEditingNode.path || '/'}${curEditingNode.title}`
       void requests
         .post('/operateApi/createFile', {
           path: curPath,
@@ -258,33 +251,63 @@ const ApiManage: FC<ApiManageProps> = () => {
     setIsModalVisible(true)
   }, [])
 
-  const handleSaveGql = (query: string) => {
-    const { parent, curr } = getNodeFamily(selectedKey, treeData)
-    const temp = {
-      title: 'new api',
-      key: Date.now(),
-    }
-    const tree = treeData ?? []
-    if (!curr || !parent) {
-      tree.push(temp)
-    } else if (curr.children) {
-      curr.children.push(temp)
-    } else {
-      parent.children.push(temp)
-    }
+  function handleClickEdit() {
+    const node = findNode(selectedKey, treeData)
+    if (!node?.path) return
 
-    setCurEditingNode(temp)
-    setTreeData([...tree])
-
-    setIsModalVisible(false)
-    setAddType('文件')
-    console.log(query)
+    void getFetcher<OperationResp>(`/operateApi/${node.id}`).then((res) => {
+      setPath(node.path)
+      setQuery(res.content)
+    })
+    setIsModalVisible(true)
   }
 
-  const handleDelete = (treeNodeKey: any) => {
+  const handleSaveGql = (query: string) => {
+    // 新增
+    if (!path) {
+      setAddType('文件')
+
+      const { parent, curr } = getNodeFamily(selectedKey, treeData)
+      const title = 'new'
+      const temp = {
+        title: title,
+        key: Date.now().toString(),
+      } as DirTreeNode
+
+      const tree = treeData ?? []
+      if (!curr || !parent) {
+        tree.push(temp)
+      } else if (curr.children) {
+        curr.children.push(temp)
+      } else {
+        // FIXME:
+        // @ts-ignore
+        parent.children.push(temp)
+      }
+
+      setCurEditingNode(temp)
+      setTreeData([...tree])
+
+      // 新增
+      void requests
+        .post('/operateApi/createFile', { title: 'new', content: query })
+        .then((_) => void message.success('保存成功'))
+
+      setAddType(null)
+    } else {
+      const curPath = path.split('/').slice(0, path.split('/').length).join('/')
+      console.log(curPath)
+      void requests
+        .put('/operateApi', { title: curPath, content: query })
+        .then((_) => void message.success('保存成功'))
+    }
+    setIsModalVisible(false)
+  }
+
+  const handleDelete = (treeNodeKey: string) => {
     const node = findNode(treeNodeKey, treeData)
     // @ts-ignore
-    requests.delete(`/operateApi/${node.path as string}`).finally(() => {
+    requests.delete(`/operateApi/${node.id}`).finally(() => {
       setRefreshFlag(!refreshFlag)
     })
   }
@@ -297,15 +320,18 @@ const ApiManage: FC<ApiManageProps> = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const handleMenuClick = (arg: any, treeNodeKey: any) => {
+  const handleMenuClick = (arg: unknown, treeNodeKey: string) => {
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     arg.domEvent.stopPropagation()
+    // @ts-ignore
     if (arg.key === '0') {
-      const { child } = findNode(treeNodeKey, treeData)
-      setCurEditingNode(child)
+      const child = findNode(treeNodeKey, treeData)
+      setCurEditingNode(child ?? null)
     }
   }
 
-  const titleRender = (nodeData: any) => {
+  const titleRender = (nodeData: DirTreeNode) => {
     const menu = (
       <Menu
         onClick={(menuInfo) => handleMenuClick(menuInfo, nodeData.key)}
@@ -322,10 +348,10 @@ const ApiManage: FC<ApiManageProps> = () => {
           {
             key: '1',
             label: (
-              <>
+              <div onClick={handleClickEdit}>
                 <IconFont type="icon-chakan" />
                 <span className="ml-1.5">编辑</span>
-              </>
+              </div>
             ),
           },
           {
@@ -376,9 +402,12 @@ const ApiManage: FC<ApiManageProps> = () => {
     )
   }
 
-  const iconRender = (nodeProps: any) => {
+  const iconRender = (nodeProps: unknown) => {
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const hasChildren = !!nodeProps.data.children
     if (hasChildren) {
+      // @ts-ignore
       if (nodeProps.expanded) {
         return <FolderOpenOutlined />
       } else {
@@ -389,28 +418,20 @@ const ApiManage: FC<ApiManageProps> = () => {
     }
   }
 
-  function connectSwitchOnChange(e) {
+  function connectSwitchOnChange(checked: boolean) {
     const node = findNode(selectedKey, treeData)
     if (!node) return
     void requests
       .put('/operateApi/rename', {
         oldPath: node.path,
         newPath: node.path,
-        enable: e,
+        enable: checked,
       })
       .then((res) => {
         if (res) {
           setRefreshFlag(!refreshFlag)
         }
       })
-  }
-
-  function handleClickEdit() {
-    const node = findNode(selectedKey, treeData)
-    if (!node?.path) return
-
-    void getFetcher(`/operateApi/${node.path as string}`).then((res) => setQuery(res.content))
-    setIsModalVisible(true)
   }
 
   const extra = (
@@ -493,6 +514,7 @@ const ApiManage: FC<ApiManageProps> = () => {
                       height: 'calc(100vh - 350px)',
                       marginBottom: '10px',
                     }}
+                    // @ts-ignore
                     titleRender={titleRender}
                     icon={iconRender}
                     // draggable
@@ -500,6 +522,7 @@ const ApiManage: FC<ApiManageProps> = () => {
                     defaultExpandAll
                     defaultSelectedKeys={['0']}
                     switcherIcon={<DownOutlined />}
+                    // @ts-ignore
                     treeData={treeData}
                     selectedKeys={[selectedKey]}
                     onSelect={handleSelectTreeNode}
@@ -525,7 +548,7 @@ const ApiManage: FC<ApiManageProps> = () => {
                         <span className="font-bold text-18px">API 管理</span>
                         <CaretRightOutlined />
                         <span className="text-16px font-bold">
-                          {(findNode(selectedKey, treeData) as DirTree)?.title ?? ''}
+                          {findNode(selectedKey, treeData)?.title ?? ''}
                         </span>
                       </div>
                       <div className="space-x-4">
@@ -545,7 +568,7 @@ const ApiManage: FC<ApiManageProps> = () => {
 
                       <div className="overflow-auto h-[calc(100vh_-_98px)]">
                         {activeKey === '0' ? (
-                          <Detail path={(findNode(selectedKey, treeData) as DirTree)?.path ?? ''} />
+                          <Detail path={findNode(selectedKey, treeData)?.path ?? ''} />
                         ) : activeKey === '1' ? (
                           <Mock />
                         ) : activeKey === '2' ? (
