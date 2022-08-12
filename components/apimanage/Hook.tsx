728b7d5f7e068d95402398a1dcfee2fc73f4bf83
bigfang <bitair@gmail.com>
2022-08-12 09:34:26 +0800

    fix typo

--- components/apimanage/Hook.tsx
+++ components/apimanage/Hook.tsx
@@ -112,7 +112,7 @@ const Hook: FC<HookProps> = ({ node }) => {
         <Editor
           height="90vh"
           defaultLanguage="typescript"
-          defaultValue="// 请编辑勾子"
+          defaultValue="// 请编辑钩子"
           value={currHook?.content}
           onChange={(value) => handleEditorChange(value)}
           className={`mt-4 ${styles.monaco}`}
