bbc40a83b9a47c67e8d65d52a0ffe2c701d3f2ea
bigfang <bitair@gmail.com>
2022-08-30 16:48:22 +0800

    增加提示

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -1,5 +1,6 @@
 import { BorderOutlined, MoreOutlined, PlayCircleOutlined, ReloadOutlined } from '@ant-design/icons'
 import { message } from 'antd'
+import { useEffect, useState } from 'react'
 import Draggable from 'react-draggable'
 
 import requests from '@/lib/fetchers'
@@ -10,18 +11,42 @@ interface Props {
   className?: string
 }
 
+type Status = '运行中' | '已停止'
+
 // eslint-disable-next-line react/prop-types
 const Player: React.FC<Props> = ({ className }) => {
+  const [status, setStatus] = useState<Status>()
+  const [refreshFlag, setRefreshFlag] = useState(false)
+
+  useEffect(() => {
+    void requests.get<unknown, Status>('api/v1/wdg/state').then(res => {
+      console.log(res)
+      setStatus(res)
+    })
+  }, [refreshFlag])
+
   function play() {
-    void requests.post('/api/v1/wdg/start').then(() => void message.success('启动成功!'))
+    if (status === '运行中') {
+      void message.warn('不能重复运行！')
+    }
+    void requests
+      .post('/api/v1/wdg/start')
+      .then(() => setRefreshFlag(!refreshFlag))
+      .then(() => void message.success('正在启动!'))
   }
 
   function reload() {
-    void requests.post('/api/v1/wdg/reStart').then(() => void message.success('重启成功!'))
+    void requests
+      .post('/api/v1/wdg/reStart')
+      .then(() => setRefreshFlag(!refreshFlag))
+      .then(() => void message.success('正在重启!'))
   }
 
   function stop() {
-    void requests.post('/api/v1/wdg/close').then(() => void message.success('停止成功!'))
+    if (status === '已停止') {
+      void message.warn('程序已经停止！')
+    }
+    void requests.post('/api/v1/wdg/close').then(() => void message.success('停止命令已发送!'))
   }
 
   return (
