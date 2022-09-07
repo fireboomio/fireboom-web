6e65b1f4823dab789d06aa357663fd6b5e4fcbb2
bigfang <bitair@gmail.com>
2022-09-07 10:49:52 +0800

    update error message

--- lib/fetchers.ts
+++ lib/fetchers.ts
@@ -23,7 +23,7 @@ requests.interceptors.response.use(
   (error: AxiosError) => {
     // @ts-ignore
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    void message.error(error?.response?.data?.message ?? '发生未知错误！')
+    void message.error(error?.response?.data?.message ?? '网络请求错误！')
     return Promise.reject(error)
   }
 )
