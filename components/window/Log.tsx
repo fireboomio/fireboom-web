b9b6f0c366df0323edabef73f1af4019b1cc96eb
bigfang <bitair@gmail.com>
2022-09-07 17:39:15 +0800

    显示日志

--- components/window/Log.tsx
+++ components/window/Log.tsx
@@ -1,5 +1,14 @@
-const Log: React.FC = () => {
-  return <>bbb</>
+type Props = {
+  children: React.ReactNode
+}
+
+// eslint-disable-next-line react/prop-types
+const Log: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="h-[calc(306px_-_28px)] overflow-auto">
+      <pre className="h-full">{children}</pre>
+    </div>
+  )
 }
 
 export default Log
--- components/window/index.tsx
+++ components/window/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { isEmpty } from 'lodash'
+import { useEffect, useState } from 'react'
+
+import { DOMAIN } from '@/lib/common'
 
 import RcTab from '../rc-tab'
 import Log from './Log'
@@ -10,11 +13,51 @@ const tabs = [
 
 const Window: React.FC = () => {
   const [tabActiveKey, setTabActiveKey] = useState('0')
+  const [log, setLog] = useState('')
+
+  useEffect(() => {
+    // setInfo({
+    //   errorInfo: { errTotal: 10, warnTotal: 22 },
+    //   engineStatus: '启动中',
+    //   hookStatus: '已停止',
+    // })
+    // return
+    void fetch(`${DOMAIN}/api/v1/wdg/log`).then(res => {
+      const reader = res.body?.getReader()
+      if (!reader) return
+
+      // @ts-ignore
+      const process = ({ value, done }) => {
+        if (done) {
+          return
+        }
+
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          const data = new Response(value)
+          void data.text().then(x =>
+            setLog(() => {
+              return isEmpty(log) ? x : `${log}\n${x}`
+            })
+          )
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.log(error)
+        }
+
+        // @ts-ignore
+        void reader.read().then(process)
+      }
+
+      // @ts-ignore
+      void reader.read().then(process)
+    })
+  }, [])
 
   return (
-    <div className="fixed w-full bottom-36px h-348px bg-[#fff] z-200 px-7 py-5 border">
+    <div className="fixed w-full bottom-36px h-348px max-h-348px bg-[#fff] z-200 px-7 py-5 border">
       <RcTab tabs={tabs} onTabClick={setTabActiveKey} activeKey={tabActiveKey} />
-      {tabActiveKey === '0' ? <Log /> : <></>}
+      {tabActiveKey === '0' ? <Log>{log}</Log> : <></>}
     </div>
   )
 }
