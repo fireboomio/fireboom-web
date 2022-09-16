fd2adc56ff29d9ecaf67ea391079fc7fd731df0c
bigfang <bitair@gmail.com>
2022-09-16 15:50:37 +0800

    增加 tab

--- components/auth/experience/index.tsx
+++ components/auth/experience/index.tsx
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import type { SWRResponse } from 'swr'
 
+import RcTab from '@/components/rc-tab'
 import { AuthListType } from '@/interfaces/auth'
 import { Connector } from '@/interfaces/connector'
 import { Experience as ExperienceType } from '@/interfaces/experience'
@@ -10,31 +11,55 @@ import { useFetchExperience } from '@/lib/service/experience'
 
 import ExperiencePreview from './ExperiencePreview'
 import ExperienceSetting from './ExperienceSetting'
+
 interface Props {
   handleTopToggleDesigner: (authType: AuthListType) => void
 }
+
+const tabs = [
+  { title: '品牌', key: '0' },
+  { title: '登录方式', key: '1' },
+  { title: '其他', key: '2' },
+]
+
 const Experience: React.FC<Props> = ({ handleTopToggleDesigner }) => {
   const experienceDataRes: SWRResponse<ExperienceType, Error> = useFetchExperience()
   const connectorsDataRes: SWRResponse<Connector[], Error> = useFetchConnector()
   const { data: experienceData } = experienceDataRes
   const { data: connectorsData } = connectorsDataRes
   const { connectorDispatch } = useContext(ConnectorContext)
+  const [tabActiveKey, setTabActiveKey] = useState('1')
 
   useEffect(() => {
     connectorDispatch({ type: 'fetchConnector', payload: connectorsData || [] })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connectorsData])
+
   return (
-    <div className="flex">
-      {experienceData && (
-        <ExperienceSetting
-          handleTopToggleDesigner={handleTopToggleDesigner}
-          data={experienceData}
-          connectorsData={connectorsData}
-        />
-      )}
-      <ExperiencePreview />
-    </div>
+    <>
+      <RcTab tabs={tabs} onTabClick={setTabActiveKey} activeKey={tabActiveKey} />
+
+      <div className="flex">
+        {tabActiveKey === '0' ? (
+          <></>
+        ) : tabActiveKey === '1' ? (
+          <>
+            {experienceData && (
+              <ExperienceSetting
+                handleTopToggleDesigner={handleTopToggleDesigner}
+                data={experienceData}
+                connectorsData={connectorsData}
+              />
+            )}
+          </>
+        ) : (
+          <></>
+        )}
+
+        <ExperiencePreview />
+      </div>
+    </>
   )
 }
+
 export default Experience
