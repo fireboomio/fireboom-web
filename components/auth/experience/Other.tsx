7c1cb51201cc0df6b2e303e628b5461e0a893b35
bigfang <bitair@gmail.com>
2022-09-19 15:13:31 +0800

    其他 接口

--- components/auth/experience/Other.tsx
+++ components/auth/experience/Other.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Switch } from 'antd'
+import { Button, Form, Input, Switch } from 'antd'
 import { useEffect, useState } from 'react'
 
 import { OtherType } from '@/interfaces/experience'
@@ -12,15 +12,16 @@ interface Props {
 // eslint-disable-next-line react/prop-types
 const Other: React.FC<Props> = ({ data }) => {
   const [state, setState] = useState<OtherType | undefined>(data)
+  const [form] = Form.useForm()
 
   useEffect(() => {
     setState(data)
   }, [data])
 
-  function save() {
-    void requests.post('/auth/brand', {
-      enabled: state?.enabled,
-      contentUrl: state?.contractUrl,
+  function handleFinish(values: OtherType) {
+    void requests.post('/auth/otherConfig', {
+      enabled: values?.enabled,
+      contentUrl: values?.contractUrl,
     })
   }
 
@@ -28,23 +29,31 @@ const Other: React.FC<Props> = ({ data }) => {
 
   return (
     <div className="w-1/2 pr-6">
-      <div className="text-xs text-[#AFB0B4] mb-4.5">使用条款</div>
-      <div className="text-sm mb-2">
-        <span className="text-[#E13D5BFF]">{'//'}</span> 开启使用条款
-      </div>
-      <div className="flex justify-between items-center py-10px px-3 bg-[#F8F9FB]">
-        <span className="pr-8 text-sm text-[#5F6269]">添加使用产品的法律协议</span>
-        <Switch className="ml-8" size="small" />
-      </div>
+      <Form form={form} name="basic" initialValues={state} onFinish={handleFinish}>
+        <div className="text-xs text-[#AFB0B4] mb-4.5">使用条款</div>
+        <div className="text-sm mb-2">
+          <span className="text-[#E13D5BFF]">{'//'}</span> 开启使用条款
+        </div>
+        <div className="flex justify-between items-center py-10px px-3 bg-[#F8F9FB]">
+          <span className="pr-8 text-sm text-[#5F6269]">添加使用产品的法律协议</span>
+          <Form.Item valuePropName="checked" name="enabled">
+            <Switch className="ml-8" size="small" />
+          </Form.Item>
+        </div>
 
-      <div className="text-sm mt-4 mb-2">
-        <span className="text-[#E13D5BFF]">{'//'}</span> 使用条款
-      </div>
-      <Input defaultValue={state.contractUrl} />
+        <div className="text-sm mt-4 mb-2">
+          <span className="text-[#E13D5BFF]">{'//'}</span> 使用条款
+        </div>
+        <Form.Item name="contractUrl">
+          <Input />
+        </Form.Item>
 
-      <Button className="float-right mt-10" type="primary" onClick={save}>
-        保存
-      </Button>
+        <Form.Item>
+          <Button className="float-right mt-10" type="primary" htmlType="submit">
+            保存
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
