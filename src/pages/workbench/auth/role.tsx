db4c59aa0f7824952132702413e83916e4f71cba
孙一帆 <>
2022-10-14 18:04:43 +0800

    权限管理页UI调整

--- src/pages/workbench/auth/role.tsx
+++ src/pages/workbench/auth/role.tsx
@@ -1,5 +1,5 @@
 import AuthRole from '@/components/auth/subs/Role'
 
 export default function AuthRolePage(){
-  return <div className="p-3"><AuthRole /></div>
+  return <div className="p-3 bg-[#fbfbfc] h-full"><AuthRole /></div>
 }
