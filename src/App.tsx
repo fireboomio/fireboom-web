45523c3539ba5356ac63ded68c95ec5070d0360a
erguotou <erguotou525@gmail.com>
2022-10-17 21:47:13 +0800

    feat: 修改角色修饰符样式

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useRoutes } from 'react-router-dom'
 import routes from '~react-pages'
 
 // import Layout from './components/layout'
-import Workbench from './components/workbench'
+// import Workbench from './components/workbench'
 
 export default function App() {
   return <Suspense fallback={<></>}>{useRoutes(routes)}</Suspense>
