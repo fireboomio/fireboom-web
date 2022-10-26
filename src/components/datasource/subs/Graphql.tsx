540bf959269d4eda6b60ba18b26ffb7315af9eb3
bigfang <bitair@gmail.com>
2022-10-26 15:05:04 +0800

    BOOM-289

--- src/components/datasource/subs/Graphql.tsx
+++ src/components/datasource/subs/Graphql.tsx
@@ -155,10 +155,7 @@ export default function Graphql({ content, type }: Props) {
         return
       case '2':
         setIsValue(true)
-        setRulesObj({
-          pattern: /^[a-zA-Z_][a-zA-Z0-9_]*$/g,
-          message: '以字母或下划线开头，只能由字母、下划线和数字组成'
-        })
+        setRulesObj({ pattern: /^\w+$/g, message: '请输入非空值' })
         return
       default:
         return
@@ -463,7 +460,7 @@ export default function Graphql({ content, type }: Props) {
                 rules={[
                   { required: true, message: '名称不能为空' },
                   {
-                    pattern: new RegExp('^[a-zA-Z_][a-zA-Z0-9_]*$', 'g'),
+                    pattern: /^[a-zA-Z_][a-zA-Z0-9_]*$/g,
                     message: '以字母或下划线开头，只能由数字、字母、下划线组成'
                   }
                 ]}
@@ -612,9 +609,7 @@ export default function Graphql({ content, type }: Props) {
                       <Form.Item wrapperCol={{ span: 16 }}>
                         <Button
                           type="dashed"
-                          onClick={() => {
-                            add({ kind: '0' })
-                          }}
+                          onClick={() => add({ kind: '0' })}
                           icon={<PlusOutlined />}
                           className="text-gray-500/60 w-1/1"
                         >
