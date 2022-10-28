e7f6d738465e9a973de4e483fe1c11049012865a
孙一帆 <>
2022-10-28 18:33:54 +0800

    新授权回调页

--- src/pages/workbench/userInfo.tsx
+++ src/pages/workbench/userInfo.tsx
@@ -1,26 +1,33 @@
 import axios from 'axios'
-import { useContext, useState } from 'react'
-
-import { ConfigContext } from '@/lib/context/ConfigContext'
+import { useState } from 'react'
 
 export default function UserInfo() {
   const [info, setInfo] = useState<string>()
 
-  const { config } = useContext(ConfigContext)
-  const apiHost =
-    config.apiHost || `${location.protocol}//${location.pathname}:${location.protocol}`
-  void axios
-    .get(`${apiHost}/app/main/auth/cookie/user`, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'application/json',
-        'WG-SDK-Version': '1.0.0-next.32'
-      }
-    })
-    .then(res => {
-      console.log('user请求结果', res.data)
-      setInfo(res.data.toString())
-    })
+  axios.get('/api/v1/oidc/userInfo').then(info => {
+    console.log(info)
+    setInfo(String(info))
+  })
+
+  // const { config } = useContext(ConfigContext)
+  // const apiHost =
+  //   config.apiHost || `${location.protocol}//${location.pathname}:${location.protocol}`
+  // const cookieId = document.cookie.replace(/(?:(?:^|.*;\s*)id\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+  // const cookieUser = document.cookie.replace(/(?:(?:^|.*;\s*)id\s*\=\s*([^;]*).*$)|^.*$/, '$1')
+  // document.cookie = `id=${cookieId}; path=/; domain=${location.pathname}:${location.protocol}`
+  // document.cookie = `user=${cookieUser}; path=/; domain=${location.pathname}:${location.protocol}`
+  // void axios
+  //   .get(`${apiHost}/app/main/auth/cookie/user`, {
+  //     withCredentials: true,
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'WG-SDK-Version': '1.0.0-next.32'
+  //     }
+  //   })
+  //   .then(res => {
+  //     console.log('user请求结果', res.data)
+  //     setInfo(res.data.toString())
+  //   })
 
   return <div>{info}</div>
 }
