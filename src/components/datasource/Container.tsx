f469eb132c24479d3a517bd27ad9571880f20446
bigfang <bitair@gmail.com>
2022-11-04 11:39:45 +0800

    BOOM-376

--- src/components/datasource/Container.tsx
+++ src/components/datasource/Container.tsx
@@ -140,9 +140,15 @@ export default function DatasourceContainer({ content, showType }: Props) {
             )}
             {content.sourceType !== 4 ? (
               <>
-                <Button className={'btn-test ml-4 mr-4'} onClick={() => testLink('bottomLeft')}>
-                  测试
-                </Button>
+                {content.sourceType === 2 ? (
+                  <a href={`/api/v1/file/downloadFile?type=1&fileName=${content.config.filePath}`}>
+                    <Button className={'btn-test ml-4 mr-4'}>测试</Button>
+                  </a>
+                ) : (
+                  <Button className={'btn-test ml-4 mr-4'} onClick={() => testLink('bottomLeft')}>
+                    测试
+                  </Button>
+                )}
                 <Button className={'btn-save mr-11'} onClick={() => handleToggleDesigner('form')}>
                   编辑
                 </Button>
