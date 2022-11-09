ec31bcf9ff9a2bdf295e10b316aa52c3b2405703
erguotou <erguotou525@gmail.com>
2022-11-09 17:03:50 +0800

    fix: 修改api页面样式问题，修复响应无法滚动的问题

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ import { argv } from 'process'
 
 // const backendUrl = 'http://120.26.62.151:9123'
 // const backendUrl = 'http://192.168.166.143:9123'
-const backendUrl = 'http://8.142.115.204:9123'
-// const backendUrl = 'http://localhost:9123'
+// const backendUrl = 'http://8.142.115.204:9123'
+const backendUrl = 'http://localhost:9123'
 
 const plugins: PluginOption[] = [
   react(),
