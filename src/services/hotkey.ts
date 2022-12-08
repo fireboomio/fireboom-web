e501ce54f98cb8f1febe663e7ab5e2090ea778c4
erguotou <erguotou525@gmail.com>
2022-12-09 00:24:29 +0800

    feat: 添加快捷键说明弹框

--- src/services/hotkey.ts
+++ src/services/hotkey.ts
@@ -1,31 +1,58 @@
 import type { HotkeysEvent } from 'hotkeys-js'
 import hotkeys from 'hotkeys-js'
 
-const hotkeyMap: Record<string, ((e: KeyboardEvent, handler: HotkeysEvent) => void)[]> = {}
+const hotkeyMap: Record<string, (e: KeyboardEvent, handler: HotkeysEvent) => void> = {}
 
 hotkeys.filter = function (event) {
-  const target = event.target || event.srcElement;
-  const tagName = target?.tagName;
-  return !(target?.isContentEditable || tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA');
+  const target = event.target || event.srcElement
+  const tagName = target?.tagName
+  console.log(
+    !(
+      (target?.isContentEditable || tagName == 'INPUT' || tagName == 'SELECT')
+      // tagName == 'TEXTAREA'
+    )
+  )
+  return !(
+    (target?.isContentEditable || tagName == 'INPUT' || tagName == 'SELECT')
+    // tagName == 'TEXTAREA'
+  )
+}
+
+export type HotkeyOptions = {
+  scope?: string
+  element?: HTMLElement | null
+  keyup?: boolean | null
+  keydown?: boolean | null
+  capture?: boolean
+  splitKey?: string
 }
 
 export function registerHotkeyHandler(
   hotkey: string,
+  options: HotkeyOptions,
   handler: (e: KeyboardEvent, handler: HotkeysEvent) => void
-) {
-  if (!hotkeyMap[hotkey]) {
-    hotkeyMap[hotkey] = []
-  }
-  hotkeyMap[hotkey].push(handler)
+): () => void
 
-  hotkeys(hotkey, handler)
+export function registerHotkeyHandler(
+  hotkey: string,
+  handler: (e: KeyboardEvent, handler: HotkeysEvent) => void
+): () => void
+
+export function registerHotkeyHandler(hotkey: string, ...args: any) {
+  const options: HotkeyOptions | undefined = args.length === 1 ? undefined : args[0]
+  const handler: (e: KeyboardEvent, handler: HotkeysEvent) => void =
+    args.length === 1 ? args[0] : args[1]
+  hotkeyMap[hotkey] = handler
+  if (options) {
+    hotkeys(hotkey, options, handler)
+  } else {
+    hotkeys(hotkey, handler)
+  }
+  console.log(`Hotkey: ${hotkey} registed`)
 
   return () => {
-    hotkeyMap[hotkey].pop()
-    if (hotkeyMap[hotkey].length) {
-      hotkeys(hotkey, hotkeyMap[hotkey][hotkeyMap[hotkey].length - 1])
-    } else {
-      hotkeys.unbind()
-    }
+    hotkeys.unbind(hotkey, hotkeyMap[hotkey])
+    delete hotkeyMap[hotkey]
+    console.log(`Hotkey: ${hotkey} unbind`)
   }
 }
