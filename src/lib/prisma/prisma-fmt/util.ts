9a256b52d03b876a053e088046d2cbb56dfe8d72
孙一帆 <>
2022-12-13 18:19:51 +0800

    prisma更新

--- src/lib/prisma/prisma-fmt/util.ts
+++ src/lib/prisma/prisma-fmt/util.ts
@@ -1,18 +1,10 @@
-/**
- * Imports
- */
-const packageJson = require('../../../package.json') // eslint-disable-line
-
 /* eslint-disable @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return */
 
 /**
  * Lookup version
  */
 export function getVersion(): string {
-  if (!packageJson || !packageJson.prisma || !packageJson.prisma.enginesVersion) {
-    return 'latest'
-  }
-  return packageJson.prisma.enginesVersion
+  return 'mock'
 }
 
 /**
@@ -20,7 +12,7 @@ export function getVersion(): string {
  * @returns Something like `2.26.0-23.9b816b3aa13cc270074f172f30d6eda8a8ce867d`
  */
 export function getEnginesVersion(): string {
-  return packageJson.dependencies['@prisma/prisma-fmt-wasm']
+  return 'mock'
 }
 
 /**
@@ -28,5 +20,5 @@ export function getEnginesVersion(): string {
  * @returns Something like `2.27.0-dev.50`
  */
 export function getCliVersion(): string {
-  return packageJson.prisma.cliVersion
+  return 'mock'
 }
