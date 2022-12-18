91e90115c419dc7f25359cba1c302cd825f72150
erguotou <erguotou525@gmail.com>
2022-12-18 22:59:28 +0800

    fix: 修改时序图

--- src/pages/workbench/apimanage/[id]/components/GraphiQL/components/SequenceDiagram/index.tsx
+++ src/pages/workbench/apimanage/[id]/components/GraphiQL/components/SequenceDiagram/index.tsx
@@ -13,6 +13,30 @@ type SequenceItem = {
   children: SequenceItem[]
 }
 
+const HEIGHT_PER_SEQUENCE = 32
+
+function getLabelAttrs(name: string) {
+  return {
+    position: {
+      distance: 0.5,
+      offset: {
+        y: -12,
+        x: 0
+      }
+    },
+    attrs: {
+      text: {
+        text: name,
+        fill: '#888',
+        fontSize: 11,
+        textAnchor: 'middle',
+        textVerticalAnchor: 'middle',
+        pointerEvents: 'none'
+      }
+    }
+  }
+}
+
 const SequenceDiagram = () => {
   const { schemaAST, schema } = useAPIManager()
   const containerRef = useRef<HTMLDivElement>(null)
@@ -48,15 +72,21 @@ const SequenceDiagram = () => {
         if (sel.kind === Kind.FIELD) {
           const children: SequenceItem[] = []
           const fieldName = sel.name.value
+          const dataSource = fieldName.split('_').shift()!
+          let found = false
           if (allQueryFields.includes(fieldName)) {
-            const dataSource = fieldName.split('_').shift()!
             list.push({ name: fieldName, children, from, to: dataSource })
+            found = true
             if (!dataSourceList.includes(dataSource)) {
               dataSourceList.push(dataSource)
             }
-            if (sel.selectionSet?.selections.length) {
-              findChildren(sel.selectionSet.selections, children, dataSource)
-            }
+          }
+          if (sel.selectionSet?.selections.length) {
+            findChildren(
+              sel.selectionSet.selections,
+              found ? children : list,
+              found ? dataSource : from
+            )
           }
         }
       }
@@ -69,6 +99,18 @@ const SequenceDiagram = () => {
       dataSourceList
     }
   }, [allQueryFields, selections])
+
+  const totalHeight = useMemo(() => {
+    let h = 72 * 2
+    function calcHeight(list: SequenceItem[]) {
+      h += list.length * HEIGHT_PER_SEQUENCE * 2
+      for (const item of list) {
+        calcHeight(item.children)
+      }
+    }
+    calcHeight(sequence.sequenceList)
+    return h
+  }, [sequence.sequenceList])
 
   useEffect(() => {
     // 块
@@ -99,7 +141,6 @@ const SequenceDiagram = () => {
         }
       }
     })
-
     // 流程线
     Graph.registerEdge('flow', {
       attrs: {
@@ -112,8 +153,19 @@ const SequenceDiagram = () => {
           }
         }
       }
+      // defaultLabel: {
+      //   position: 4,
+      //   attrs: {
+      //     text: {
+      //       fill: '#888',
+      //       fontSize: 11,
+      //       textAnchor: 'middle',
+      //       textVerticalAnchor: 'middle',
+      //       pointerEvents: 'none'
+      //     }
+      //   }
+      // }
     })
-
     // 流程虚线
     Graph.registerEdge('dash-flow', {
       attrs: {
@@ -138,131 +190,164 @@ const SequenceDiagram = () => {
     }
   }, [])
 
-  const totalHeight = useMemo(
-    () => 100 * (2 + sequence.dataSourceList.length) - 35,
-    [sequence.dataSourceList.length]
-  )
-
   useEffect(() => {
     if (selections.length) {
-      // 初始化画布
-      const graph = new Graph({
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        container: containerRef.current!,
-        mousewheel: {
-          enabled: true,
-          zoomAtMousePosition: true,
-          modifiers: 'ctrl',
-          minScale: 0.5,
-          maxScale: 3
-        },
-        interacting: {
-          nodeMovable: false,
-          edgeMovable: false,
-          edgeLabelMovable: false,
-          arrowheadMovable: false,
-          vertexMovable: false,
-          useEdgeTools: false
-        },
-        autoResize: true
-      })
-
-      // 初始的客户端/服务端
-      const dataSourceNodes: Node[] = [
-        graph.createNode({
-          shape: 'block',
-          label: '客户端',
-          x: 12,
-          y: 12
-          // ports: {
-          //   groups: { g: { position: 'bottom', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
-        }),
-        graph.createNode({
-          shape: 'block',
-          label: '客户端',
-          x: 12,
-          y: totalHeight - 35
-          // ports: {
-          //   groups: { g: { position: 'top', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
-        }),
-        graph.createNode({
-          shape: 'block',
-          label: '服务端',
-          x: 124,
-          y: 12
-          // ports: {
-          //   groups: { g: { position: 'bottom', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
-        }),
-        graph.createNode({
-          shape: 'block',
-          label: '服务端',
-          x: 124,
-          y: totalHeight - 35
-          // ports: {
-          //   groups: { g: { position: 'top', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
+      setTimeout(() => {
+        // 初始化画布
+        const graph = new Graph({
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          container: containerRef.current!,
+          mousewheel: {
+            enabled: true,
+            zoomAtMousePosition: true,
+            modifiers: 'ctrl',
+            minScale: 0.5,
+            maxScale: 3
+          },
+          interacting: {
+            nodeMovable: false,
+            edgeMovable: false,
+            edgeLabelMovable: false,
+            arrowheadMovable: false,
+            vertexMovable: false,
+            useEdgeTools: false
+          },
+          autoResize: true
         })
-      ]
 
-      // 关联到的数据源
-      for (const [index, seq] of sequence.dataSourceList.entries()) {
-        const node = graph.createNode({
-          shape: 'block',
-          label: seq,
-          x: 20 + 112 * (index + 2),
-          y: 12
-          // ports: {
-          //   groups: { g: { position: 'bottom', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
+        // 初始的客户端/服务端
+        const dataSourceNodes: Node[] = [
+          graph.createNode({
+            shape: 'block',
+            label: '客户端',
+            x: 12,
+            y: 12
+          }),
+          graph.createNode({
+            shape: 'block',
+            label: '客户端',
+            x: 12,
+            y: totalHeight - 40
+          }),
+          graph.createNode({
+            shape: 'block',
+            label: '服务端',
+            x: 148,
+            y: 12
+          }),
+          graph.createNode({
+            shape: 'block',
+            label: '服务端',
+            x: 148,
+            y: totalHeight - 40
+          })
+        ]
+
+        // 关联到的数据源
+        for (const [index, seq] of sequence.dataSourceList.entries()) {
+          const node = graph.createNode({
+            shape: 'block',
+            label: seq,
+            x: 20 + 136 * (index + 2),
+            y: 12
+          })
+          dataSourceNodes.push(node)
+          const eNode = graph.createNode({
+            shape: 'block',
+            label: seq,
+            x: 20 + 136 * (index + 2),
+            y: totalHeight - 40
+          })
+          dataSourceNodes.push(eNode)
+        }
+
+        graph.addNodes([...dataSourceNodes])
+
+        // 上下连线
+        for (let i = 0; i < dataSourceNodes.length; i = i + 2) {
+          graph.addEdge({
+            shape: 'block-connector',
+            source: dataSourceNodes[i],
+            target: dataSourceNodes[i + 1]
+          })
+        }
+
+        // 请求去程 客户端-服务端
+        const e1 = graph.addEdge({
+          shape: 'flow',
+          source: { x: 52, y: 64 },
+          target: { x: 188, y: 64 }
         })
-        dataSourceNodes.push(node)
-        const eNode = graph.createNode({
-          shape: 'block',
-          label: seq,
-          x: 20 + 112 * (index + 2),
-          y: totalHeight - 35
-          // ports: {
-          //   groups: { g: { position: 'top', attrs: { circle: { r: 0 } } } },
-          //   items: [{ id: 'p', group: 'g' }]
-          // }
+        e1.appendLabel(getLabelAttrs('发起请求'))
+
+        let y = 56
+        console.log(sequence.sequenceList)
+
+        // eslint-disable-next-line no-inner-declarations
+        function loopSequence(list: SequenceItem[]) {
+          // 请求去程
+          for (const seq of list) {
+            y += HEIGHT_PER_SEQUENCE
+            let source: Node
+            let target: Node
+            if (!seq.from) {
+              source = dataSourceNodes[2]
+            } else {
+              const index = sequence.dataSourceList.indexOf(seq.from)
+              source = dataSourceNodes[index * 2 + 4]
+            }
+            const toIndex = sequence.dataSourceList.indexOf(seq.to)
+            target = dataSourceNodes[toIndex * 2 + 4]
+            const edge = graph.addEdge({
+              shape: 'flow',
+              source: { x: source.getBBox().x + 40, y },
+              target: { x: target.getBBox().x + 40, y }
+            })
+            if (seq.name) {
+              edge.appendLabel(getLabelAttrs(seq.name))
+            }
+          }
+          for (const seq of list) {
+            if (seq.children.length) {
+              loopSequence(seq.children)
+            }
+          }
+          // 请求回程
+          for (const seq of list) {
+            y += HEIGHT_PER_SEQUENCE
+            let source: Node
+            let target: Node
+            if (!seq.from) {
+              target = dataSourceNodes[2]
+            } else {
+              const index = sequence.dataSourceList.indexOf(seq.from)
+              target = dataSourceNodes[index * 2 + 4]
+            }
+            const toIndex = sequence.dataSourceList.indexOf(seq.to)
+            source = dataSourceNodes[toIndex * 2 + 4]
+            const edge = graph.addEdge({
+              shape: 'dash-flow',
+              source: { x: source.getBBox().x + 40, y },
+              target: { x: target.getBBox().x + 40, y }
+            })
+            if (seq.name) {
+              edge.appendLabel(getLabelAttrs(seq.name))
+            }
+          }
+        }
+        loopSequence(sequence.sequenceList)
+
+        // 请求回程 服务端-客户端
+        y += HEIGHT_PER_SEQUENCE
+        const e2 = graph.addEdge({
+          shape: 'dash-flow',
+          source: { x: 188, y },
+          target: { x: 52, y }
         })
-        dataSourceNodes.push(eNode)
-      }
+        e2.appendLabel(getLabelAttrs('请求结果'))
 
-      graph.addNodes([...dataSourceNodes])
-
-      // 上下连线
-      for (let i = 0; i < dataSourceNodes.length; i = i + 2) {
-        graph.addEdge({
-          shape: 'block-connector',
-          source: dataSourceNodes[i],
-          target: dataSourceNodes[i + 1]
-        })
-      }
-
-      // 请求去程
-      graph.addEdge({ shape: 'flow', source: { x: 52, y: 56 }, target: { x: 164, y: 56 } })
-
-      graph.addEdge({ shape: 'dash-flow', source: { x: 164, y: 108 }, target: { x: 52, y: 108 } })
-
-      // 请求回程
-
-      // graph.addEdges([
-      //   { shape: 'block-connector', source: client1, target: client2 },
-      //   { shape: 'block-connector', source: server1, target: server2 },
-      //   { shape: 'flow', source: { x: 52, y: 56 }, target: { x: 164, y: 56 } },
-      //   { shape: 'dash-flow', source: { x: 164, y: 108 }, target: { x: 52, y: 108 } }
-      // ])
-
-      // return () => graph.dispose()
+        // return graph.dispose
+      }, 100)
     }
   }, [totalHeight, selections, sequence.dataSourceList, sequence.sequenceList])
 
@@ -270,10 +355,10 @@ const SequenceDiagram = () => {
     <div className="bg-white rounded shadow p-2">
       <div
         ref={containerRef}
-        className="min-w-120 min-h-80"
+        // className="min-w-60 min-h-40"
         style={{
-          width: `${40 + (2 + sequence.dataSourceList.length) * 112}px`,
-          height: `${totalHeight + 40}px`
+          width: `${40 + (2 + sequence.dataSourceList.length) * 136}px`,
+          height: `${totalHeight}px`
         }}
       >
         {!selections.length && <div className="p-4">暂无内容</div>}
