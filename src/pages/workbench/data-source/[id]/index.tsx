c0ee5d518d08448c5d8679600280217d4adee916
erguotou <erguotou525@gmail.com>
2023-02-03 00:33:28 +0800

    chore: remove unused

--- src/pages/workbench/data-source/[id]/index.tsx
+++ src/pages/workbench/data-source/[id]/index.tsx
@@ -1,18 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useEffect, useReducer, useState } from 'react'
-import { Helmet } from 'react-helmet'
-import { useNavigate, useParams } from 'react-router-dom'
-import useSWRImmutable from 'swr/immutable'
-import { useImmer } from 'use-immer'
+import { useContext } from 'react'
 
-import type { DatasourceResp, ShowType } from '@/interfaces/datasource'
-import {
-  DatasourceDispatchContext,
-  DatasourceToggleContext
-} from '@/lib/context/datasource-context'
-import { WorkbenchContext } from '@/lib/context/workbenchContext'
-import requests from '@/lib/fetchers'
-import datasourceReducer from '@/lib/reducers/datasource-reducer'
+import { DatasourceToggleContext } from '@/lib/context/datasource-context'
 
 import DatasourceContainer from '../components/Container'
 
