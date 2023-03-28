20a7ff12c49a0438da610631750a6a438580c24e
woodensail <woodensail@hotmail.com>
2023-03-28 16:13:41 +0800

    强制向编辑器注入全局钩子

--- src/hooks/store/hook/model.ts
+++ src/hooks/store/hook/model.ts
@@ -1,12 +1,35 @@
+import { uniqBy } from 'lodash'
 import { mutate } from 'swr'
 import useSWRImmutable from 'swr/immutable'
 
 import requests from '@/lib/fetchers'
 
+const staticInject = {
+  auth: ['mutatingPostAuthentication', 'postAuthentication', 'postLogout', 'revalidate'],
+  global: ['onRequest', 'onResponse', 'onConnectionInit']
+}
+
+type StaticInjectKeys = keyof typeof staticInject
+
 export function useHookModel(fetch = true) {
-  return useSWRImmutable<any[]>(fetch ? '/hook/model' : null, requests.get, {
+  const data = useSWRImmutable<any[]>(fetch ? '/hook/model' : null, requests.get, {
     revalidateOnMount: true
   }).data
+  if (data) {
+    Object.keys(staticInject).forEach(key => {
+      const target = data.find(x => x.name === key)
+      if (target?.children) {
+        staticInject[key as StaticInjectKeys].forEach(hook => {
+          target.children.push({
+            name: hook,
+            path: `${key}/${hook}`
+          })
+        })
+        target.children = uniqBy(target.children, 'name')
+      }
+    })
+  }
+  return data
 }
 
 export function mutateHookModel() {
