c9a8ef904f4014c8330d8281c31aa6a9cc69f268
woodensail <woodensail@hotmail.com>
2023-04-10 17:00:01 +0800

    vscode-web静态资源

---
+++ public/vscode/vscode-web/dist/out/vs/editor/common/services/editorSimpleWorker.nls.js
@@ -0,0 +1,5 @@
+"use strict";/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/b7886d7461186a5eac768481578c1d7ca80e2d21/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
