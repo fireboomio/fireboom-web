a7b91fb713dd944b1c0703346474929ba5a42fdb
woodensail <woodensail@hotmail.com>
2023-04-12 16:03:27 +0800

    vscode文件系统

--- public/vscode/fb-fs/extension.js
+++ public/vscode/fb-fs/extension.js
@@ -15,60 +15,86 @@ class VirtualFileSystemProvider {
   constructor() {}
 
   async readFile(uri) {
-    return fetch(`/api/v1/vscode/readFile?uri=${trimPath(uri.path)}`)
-      .then(resp => resp.json())
-      .then(resp => {
-        return new TextEncoder().encode(resp)
-      })
+    return fetch(`/api/v1/vscode/readFile?uri=${trimPath(uri.path)}`).then(async resp => {
+      return new Uint8Array(await resp.arrayBuffer())
+    })
   }
 
   async writeFile(uri, content, options) {
+    // 将 ArrayBuffer 转换为 Blob 对象
+    const dataBlob = new Blob([content], { type: 'application/octet-stream' })
+
+    // 使用 FormData 对象来传递二进制数据
+    const formData = new FormData()
+    formData.append('content', dataBlob, 'example.bin')
+    formData.append('uri', trimPath(uri.path))
+    formData.append('create', options.create)
+    formData.append('overwrite', options.overwrite)
     return fetch(`/api/v1/vscode/writeFile`, {
       method: 'post',
-      body: JSON.stringify({
-        uri: trimPath(uri.path),
-        content: content
-      })
+      body: formData
+    }).then(resp => {
+      const json = resp.json()
+      if (options.create) {
+        this._fireSoon({ type: vscode.FileChangeType.Created, uri })
+      } else {
+        this._fireSoon({ type: vscode.FileChangeType.Changed, uri })
+      }
     })
-      .then(resp => resp.json())
   }
 
   async delete(uri, options) {
     return fetch(`/api/v1/vscode/delete`, {
       method: 'delete',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
       body: JSON.stringify({
         uri: trimPath(uri.path),
         recursive: options.recursive
       })
+    }).then(resp => {
+      let dirname = uri.with({ path: this._dirname(uri.path) })
+      this._fireSoon(
+        { type: vscode.FileChangeType.Changed, uri: dirname },
+        { uri, type: vscode.FileChangeType.Deleted }
+      )
     })
-      .then(resp => resp.json())
   }
 
   async rename(oldUri, newUri, options) {
     return fetch(`/api/v1/vscode/rename`, {
       method: 'put',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
       body: JSON.stringify({
         oldUri: trimPath(oldUri.path),
         newUri: trimPath(newUri.path),
         overwrite: options.overwrite
       })
+    }).then(resp => {
+      this._fireSoon(
+        { type: vscode.FileChangeType.Deleted, uri: oldUri },
+        { type: vscode.FileChangeType.Created, uri: newUri }
+      )
     })
-      .then(resp => resp.json())
   }
 
   async stat(uri) {
     return fetch(`/api/v1/vscode/state?uri=${trimPath(uri.path)}`)
-      .then(resp => resp.json()).then(resp => {
+      .then(resp => resp.json())
+      .then(resp => {
         console.log('stat', resp)
         return resp
-      }).catch(resp => {
-        throw vscode.FileSystemError.FileNotFound(uri);
+      })
+      .catch(resp => {
+        throw vscode.FileSystemError.FileNotFound(uri)
       })
   }
 
   async readDirectory(uri) {
-    return fetch(`/api/v1/vscode/readDirectory?uri=${trimPath(uri.path)}`)
-      .then(resp => resp.json())
+    return fetch(`/api/v1/vscode/readDirectory?uri=${trimPath(uri.path)}`).then(resp => resp.json())
   }
 
   async createDirectory(uri) {
@@ -76,11 +102,19 @@ class VirtualFileSystemProvider {
     // 这里实现创建目录的逻辑
     return fetch(`/api/v1/vscode/createDirectory`, {
       method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
       body: JSON.stringify({
         uri: trimPath(uri.path)
       })
+    }).then(resp => {
+      let dirname = uri.with({ path: this._dirname(uri.path) })
+      this._fireSoon(
+        { type: vscode.FileChangeType.Changed, uri: dirname },
+        { type: vscode.FileChangeType.Created, uri }
+      )
     })
-      .then(resp => resp.json())
   }
 
   watch(uri, options) {
@@ -99,6 +133,41 @@ class VirtualFileSystemProvider {
       this._emitter.fire(this._bufferedEvents)
       this._bufferedEvents.length = 0
     }, 5)
+  }
+  _dirname(path) {
+    path = this._rtrim(path, '/')
+    if (!path) {
+      return '/'
+    }
+
+    return path.substr(0, path.lastIndexOf('/'))
+  }
+  _rtrim(haystack, needle) {
+    if (!haystack || !needle) {
+      return haystack
+    }
+
+    const needleLen = needle.length,
+      haystackLen = haystack.length
+
+    if (needleLen === 0 || haystackLen === 0) {
+      return haystack
+    }
+
+    let offset = haystackLen,
+      idx = -1
+
+    while (true) {
+      idx = haystack.lastIndexOf(needle, offset - 1)
+      if (idx === -1 || idx + needleLen !== offset) {
+        break
+      }
+      if (idx === 0) {
+        return ''
+      }
+      offset = idx
+    }
+    return haystack.substring(0, offset)
   }
 }
 
