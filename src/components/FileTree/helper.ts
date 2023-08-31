74ae69481dc619d94e6ec5b5e8704128f09c7ce6
erguotou <erguotou525@gmail.com>
2023-08-31 18:16:48 +0800

    chore: update

--- src/components/FileTree/helper.ts
+++ src/components/FileTree/helper.ts
@@ -1,5 +1,15 @@
 import type { InnerNode } from './types'
 
+function travelNode(node: InnerNode[], map: Record<string, InnerNode>) {
+  for (const i of node) {
+    if (i.children) {
+      travelNode(i.children, map)
+    } else if (i.pos) {
+      map[i.pos] = i
+    }
+  }
+}
+
 export function getTreeItemsBetween(start: string, end: string, treeData: InnerNode[]): string[] {
   // pos 是 0-M[-N] 的格式
   if (start && end && treeData.length) {
@@ -11,7 +21,9 @@ export function getTreeItemsBetween(start: string, end: string, treeData: InnerN
       .split('-')
       .map(i => parseInt(i))
       .slice(1)
-    // for (let i = 0; i < startArr)
+
+    const nodeMap: Record<string, InnerNode> = {}
+    travelNode(treeData, nodeMap)
   }
   return []
 }
