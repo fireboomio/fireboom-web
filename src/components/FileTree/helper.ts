a272c56580326fdd9de635135f3e825da0043c25
erguotou <erguotou525@gmail.com>
2023-09-01 09:41:01 +0800

    feat: 支持多选

--- src/components/FileTree/helper.ts
+++ src/components/FileTree/helper.ts
@@ -4,11 +4,11 @@ function flatten(node: InnerNode[], level: string) {
   const arr: InnerNode[] = []
   for (const [index, item] of node.entries()) {
     if (item.children?.length) {
-      arr.push(...flatten(item.children, `${level}-`))
+      arr.push(...flatten(item.children, `${level}-${index}`))
     } else {
       arr.push({
         ...item,
-        pos: level ? `0-${level}-${index}` : `0-${index}`
+        pos: `${level}-${index}`
       })
     }
   }
@@ -18,23 +18,16 @@ function flatten(node: InnerNode[], level: string) {
 export function getTreeItemsBetween(start: string, end: string, treeData: InnerNode[]): string[] {
   // pos 是 0-M[-N] 的格式
   if (start && end && treeData.length) {
-    // const startArr = start
-    //   .split('-')
-    //   .map(i => parseInt(i))
-    //   .slice(1)
-    // const endArr = end
-    //   .split('-')
-    //   .map(i => parseInt(i))
-    //   .slice(1)
-
-    const nodeArr = flatten(treeData, '')
+    const nodeArr = flatten(treeData, '0')
 
     const startIndex = nodeArr.findIndex(node => node.pos === start)
     const endIndex = nodeArr.findIndex(node => node.pos === end)
-    if (startIndex > endIndex) {
-      return nodeArr.slice(endIndex, startIndex).map(item => item.key)
-    } else {
-      return nodeArr.slice(startIndex, endIndex).map(item => item.key)
+    if (startIndex > -1 && endIndex > -1) {
+      if (startIndex > endIndex) {
+        return nodeArr.slice(endIndex, startIndex + 1).map(item => item.key)
+      } else {
+        return nodeArr.slice(startIndex, endIndex + 1).map(item => item.key)
+      }
     }
   }
   return []
