7be41bfcc771b2a71e96c9bad7d1091657434a04
erguotou <erguotou525@gmail.com>
2023-10-07 17:00:26 +0800

    chore: 样式调整

--- src/pages/workbench/apimanage/[...path]/components/GraphiQL/components/DirectivePopup/DirectiveDescription.tsx
+++ src/pages/workbench/apimanage/[...path]/components/GraphiQL/components/DirectivePopup/DirectiveDescription.tsx
@@ -18,11 +18,11 @@ const DirectiveDescription = ({ name, title, code }: DirectiveDescriptionProps) 
       <div className="flex-shrink-0 text-xs text-[#6F6F6F]">
         {name} {title}, <FormattedMessage defaultMessage="参考用法如下" />
       </div>
-      <code className="bg-[#F8F9FD] flex-1 mt-2 text-default text-xs px-2 pt-6 pb-2 leading-5 block relative overflow-hidden">
-        <pre className="h-full mb-0 w-full overflow-auto">{code}</pre>
+      <code className="bg-[#F8F9FD] flex-1 mt-2 text-default text-xs px-2 pt-4.5 pb-2 leading-5 block relative overflow-hidden">
+        <pre className="h-full my-0 w-full overflow-auto">{code}</pre>
 
         <CopyOutlined
-          className="cursor-pointer text-xs top-3 right-3 absolute"
+          className="cursor-pointer text-xs top-1.5 right-1.5 absolute"
           onClick={() => {
             copy(code)
             message.success(intl.formatMessage({ defaultMessage: '复制成功' }))
